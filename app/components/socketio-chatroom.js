9ea46b31b263044dd9ff8c46a506592dee29e8fc
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-10 12:01:03 -0600

    created functionality to interact with GO socket.io server

--- app/components/socketio-chatroom.js
+++ app/components/socketio-chatroom.js
@@ -6,34 +6,39 @@ export default Ember.Component.extend({
   compInput: '',
   messagesfoo: [],
   messagesbar: [],
-  currUser: `guest${Math.ceil(Math.random() * 9999)}`,
+  currUser: Ember.computed(function(){
+    return `guest${Math.ceil(Math.random() * 9999)}`
+  }),
   room: 'foo',
   roomInput: '',
-  setSockets(room){
-    let socket = this.get('socketIOService').socketFor(`ws://localhost:7000/${this.get('room')}`)
-    socket.on('open', ()=>{
-        console.log(this.get('currUser'));
-        socket.send(`${this.get('currUser')} has joined the room.`);
-    }, this);
-    socket.on('close', ()=>{
-      socket.send(`${this.get('currUser')} has left the room.`);
-    }, this);
+  setSockets(){
+    let socket = this.get('socketIOService').socketFor(`ws://localhost:7000/`);
+    socket.emit("open", [
+      this.get('currUser'),
+      this.get('room')
+    ]);
     socket.on('message', (message)=>{
+      console.log(message);
       this.set(`messages${this.get('room')}`, this.get(`messages${this.get('room')}`).concat(message))
     })
     this.get('actions')[`submitText`] = function(){
-      socket.send(`${this.get('currUser')}: ${this.get(`compInput`)}`, true);
+      socket.emit("message", [
+        this.get('room'),
+        `${this.get('currUser')}: ${this.get(`compInput`)}`
+      ]);
       this.set(`compInput`, '')
     }
   },
   init(){
       this._super(...arguments);
-      this.setSockets(this.get('room'))
-      console.log(this.get('currUser'));
+      this.setSockets()
   },
   actions: {
     changeRoom: function(){
       this.set('room', this.get('roomInput'));
+      this.get('socketIOService').closeSocketFor('ws://localhost:7000/');
+      this.setSockets();
+      this.set('roomInput', '');
     }
   }
 });
